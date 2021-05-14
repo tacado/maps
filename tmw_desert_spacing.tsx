202c4d98eb99e2d8d055441943ed4395e6ed86f2
tacado <84180628+tacado@users.noreply.github.com>
2021-05-14 12:12:44 +0200

    Add files via upload

---
+++ tmw_desert_spacing.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="../../../../../../Program Files/Tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="0" y="15.75" width="31.9598" height="16.0948"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.114963" y="14.9452" width="16.7846" height="16.8996"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.40327" y="4.71349" width="21.1532" height="20.5784">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
